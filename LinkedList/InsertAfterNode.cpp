2142c4fb317fda573273583c5788f6043be49433
Rishabh Sharma <riddletom76@gmail.com>
2019-03-24 17:34:08 +0530

    added

--- LinkedList/InsertAfterNode.cpp
+++ LinkedList/InsertAfterNode.cpp
@@ -7,7 +7,7 @@ class Node{
     Node(){
         next = NULL;
     }
-}
+};
 void PrintLinkedList(Node* head){
     while(head!=NULL){
         cout<<head->data<<" ";
@@ -15,8 +15,15 @@ void PrintLinkedList(Node* head){
     }
     cout<<endl;
 }
-Node* InsertAfter(Node* prevNode, int data){
-
+void InsertAfter(Node* prevNode, int data){
+    Node *newNode = new Node();
+    newNode->data = data;
+    if(prevNode == NULL){
+        prevNode = newNode;
+        return;
+    }
+    newNode->next = prevNode->next;
+    prevNode->next = newNode;
 }
 int main(int argc, char const *argv[])
 {
@@ -29,16 +36,17 @@ int main(int argc, char const *argv[])
     Node* c = new Node();
     b->next = c;
     c->data = 50;
-    Node* head = a;
+    Node* head = NULL;
+    head = a;
     PrintLinkedList(head);
     InsertAfter(a,20);
     cout<<"inserted 20"<<endl;
     PrintLinkedList(head);
     InsertAfter(b,40);
     cout<<"inserted 40"<<endl;
+    PrintLinkedList(head);
     InsertAfter(c,60);
     cout<<"inserted 60"<<endl;
     PrintLinkedList(head);
     return 0;
 }
-
