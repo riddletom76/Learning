da55327f234c81cb2ad4aca8a8e615ed725f4c97
Rishabh Sharma <riddletom76@gmail.com>
2019-03-24 17:14:42 +0530

    added a method to push data in the front of the linked list

--- LinkedList/InsertingNode.cpp
+++ LinkedList/InsertingNode.cpp
@@ -1,5 +1,6 @@
 //https://www.geeksforgeeks.org/linked-list-set-2-inserting-a-node/
-#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
 class Node{
     public:
     int data;
@@ -16,6 +17,15 @@ Node* InsertFront(Node* head, Node* newNode){
     head = newNode;
     return head;
 }
+
+Node* PushData(Node* head, int newData){
+Node* newNode = new Node();
+newNode->data = newData;
+newNode->next = head;
+//head->next = newNode;
+return newNode;
+}
+
 void PrintLinkedList(Node* head){
     while(head!=NULL){
         cout<<head->data<<" ";
@@ -23,20 +33,34 @@ void PrintLinkedList(Node* head){
     }
     cout<<endl;
 }
-int main(){
-    Node* linkedList = NULL;
-    Node* node1 = new Node();
-    Node* node2 = new Node();
-    Node* node3 = new Node();
-    node1->data = 10;
-    node2->data = 20;
-    node1->next = node2;
-    node3->data = 30;
-    node2->next = node3;
-    linkedList = node1;
-    Node *newNode = new Node();
-    newNode->data = 9;
-    linkedList = InsertFront(linkedList,newNode);
-    PrintLinkedList(linkedList);
+// int main(){
+//     Node* linkedList = NULL;
+//     Node* node1 = new Node();
+//     Node* node2 = new Node();
+//     Node* node3 = new Node();
+//     node1->data = 10;
+//     node2->data = 20;
+//     node1->next = node2;
+//     node3->data = 30;
+//     node2->next = node3;
+//     linkedList = node1;
+//     Node *newNode = new Node();
+//     newNode->data = 9;
+//     linkedList = InsertFront(linkedList,newNode);
+//     PrintLinkedList(linkedList);
+//     return 0;
+// }
+
+int main()
+{
+    /* code */
+    Node* a = new Node();
+    Node* b = new Node();
+    a->data = 10;
+    b->data = 20;
+    a->next = b;
+    Node* head = a;
+    head = PushData(head, 9);
+    PrintLinkedList(head);
     return 0;
 }
