ea81ef897b5f79dbf4d2e89095492abd18107d29
Rishabh Sharma <riddletom76@gmail.com>
2019-03-24 17:45:56 +0530

    to append data to a linked list

---
+++ LinkedList/AppendData.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(){
+        next = NULL;
+    }
+};
+Node* Append(Node* head, int data){
+    Node* newNode = new Node();
+    newNode->data = data;
+    if(head == NULL){
+        head = newNode;
+    }else{
+        Node* temp = head;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    return head;
+}
+void PrintLinkedList(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+int main(int argc, char const *argv[])
+{
+    Node* a = new Node();
+    a->data = 10;
+    Node* b = new Node();
+    a->next = b;
+    b->data = 20;
+    Node* head = NULL;
+    head = Append(head,1);
+    head = a;
+    head = Append(head, 30);
+    PrintLinkedList(head);
+    return 0;
+}
