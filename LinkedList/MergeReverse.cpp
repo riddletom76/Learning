1399ddd7dc4b3305553e66608b125d8306c0258d
Rishabh <riddletom76@gmail.com>
2019-06-09 15:00:51 +0530

    remove the reverse method, now sorting directly from the Merge method

--- LinkedList/MergeReverse.cpp
+++ LinkedList/MergeReverse.cpp
@@ -1,65 +1,61 @@
-// a: 5->10->15->40
-// b: 2->3->20 
-
+//my solution for - https://www.geeksforgeeks.org/merge-two-sorted-linked-lists-such-that-merged-list-is-in-reverse-order/
 #include<iostream>
 #include"LinkedList.h"
 using namespace std;
 
-Node* Reverse(Node* head){
-	Node* curr = head;
-	Node* prev = NULL;
-	while(curr!=NULL){
-		Node* temp = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = temp;
-	}
-	return prev;
-}
-
 Node* Merge(Node* head1, Node* head2){
-	if(head1 == NULL){
-		return Reverse(head2);
-	}
-	if(head2 == NULL){
-		return Reverse(head1);
-	}
 	Node* temp1 = head1;
 	Node* temp2 = head2;
 	Node* result = NULL;
-	Node* resultEnd = NULL;
-	while(temp1!=NULL && temp2!=NULL){
-		if(temp1->data<=temp2->data){
-			if(result==NULL){
+	while((temp1 != NULL) && (temp2 != NULL)){
+		Node* temp = NULL;
+		if(temp1->data <= temp2->data){
+			temp = temp1->next;
+			if(result == NULL){
 				result = temp1;
-				resultEnd = result;
-			}else{
-				resultEnd->next = temp1;
-				resultEnd = temp1;
+				temp1->next = NULL;
 			}
-			temp1 = temp1->next;
+			else{
+				temp1->next = result;
+				result = temp1;
+			}
+			temp1 = temp;
 		}else{
+			temp = temp2->next;
 			if(result == NULL){
 				result = temp2;
-				resultEnd = result;
-			}else{
-				resultEnd->next = temp2;
-				resultEnd = temp2;
+				temp2->next = NULL;
 			}
-			temp2 = temp2->next;
+			else{
+				temp2->next = result;
+				result = temp2;
+			}
+			temp2 = temp;
 		}
 	}
 	while(temp1!=NULL){
-		resultEnd->next = temp1;
-		resultEnd = temp1;
-		temp1 = temp1->next;
+		Node* temp = temp1->next;
+		if(result == NULL){
+			result = temp1;
+			temp1->next = NULL;
+		}else{
+			temp1->next = result;
+			result = temp1;
+		}
+		temp1 = temp;
 	}
 	while(temp2!=NULL){
-		resultEnd->next = temp2;
-		resultEnd = temp2;
-		temp2 = temp2->next;
+		Node* temp = temp2->next;
+		if(result == NULL){
+			result = temp2;
+			temp2->next = NULL;
+		}else{
+			temp2->next = result;
+			result = temp2;
+		}
+		temp2 = temp;
 	}
-	return Reverse(result);
+	return result;
 }
 int main(){
 	Node* head1 = NULL;
