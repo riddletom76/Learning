c0aa36c1dfa7a1437149dd45c08f25d71e148128
Rishabh <riddletom76@gmail.com>
2019-06-11 07:46:19 +0530

    add alternate way to reverse

--- LinkedList/ReverseNAlternate.cpp
+++ LinkedList/ReverseNAlternate.cpp
@@ -2,6 +2,7 @@
 #include"LinkedList.h"
 using namespace std;
 
+//Revusive Solution
 Node* _Alternate(Node* head, int key, bool toReverse){
 	if(head == NULL){
 		return NULL;
@@ -33,6 +34,45 @@ Node* AlterNate(Node* head, int key){
 	return _Alternate(head, key, true);
 }
 
+
+//Alternate Solution
+
+Node* AlternateReverse(Node* head, int key){
+	Node* curr = head;
+	Node* prev = NULL;
+	Node* next;
+	int count = 1;
+	while((count<=key) && (curr!=NULL)){
+		count++;
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+		cout<<"count "<<count<<endl;
+		cout<<"curr ";
+		PrintLinkedList(curr);
+		cout<<"prev ";
+		PrintLinkedList(prev);
+	}
+	cout<<"curr ";
+	PrintLinkedList(curr);
+	cout<<"prev ";
+	PrintLinkedList(prev);
+	if(head!=NULL){
+		head->next = curr;
+	}
+	count = 1;
+	while((curr!=NULL) && (count<key)){
+		curr = curr->next;
+		count++;
+	}
+	cout<<"curr after skipping"<<endl;
+	PrintLinkedList(curr);
+	if(curr!=NULL){
+		curr->next = AlternateReverse(curr->next, key);
+	}
+	return prev;
+}
 int main(){
 	Node* head = NULL;
 	head = Push(head, 10);
@@ -46,5 +86,6 @@ int main(){
 	head = Push(head, 90);
 	PrintLinkedList(head);
 	head = AlterNate(head, 3);
+	head = AlternateReverse(head,3);
 	PrintLinkedList(head);
 }
