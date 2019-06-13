c3bd912f144c45b3b1790d61ab6d27c8afaa5421
Rishabh <riddletom76@gmail.com>
2019-06-14 05:19:18 +0530

    Alternate odd even

--- LinkedList/AlternateOddEven.cpp
+++ LinkedList/AlternateOddEven.cpp
@@ -3,65 +3,53 @@
 using namespace std;
 
 Node* AlternateOddEven(Node* head){
-	Node* temp = head;
-	Node* newHead = NULL;
-	Node* evenHead = NULL;
-	Node* evenTail = NULL;
-	Node* newTail = NULL;
-	Node* prev = NULL;
+	Node* temp = head->next;
+	Node* prev = head;
+	Node* even = NULL;
 	while(temp!=NULL){
 		Node* next = temp->next;
 		if(temp->data%2!=0){
-			if(newHead = NULL){
-				newHead = temp;
-				newTail = newHead;
-			}else{
-				temp->next= NULL;
-				newTail->next = temp;
-				newTail = temp;
-			}
-			prev = temp;
-			temp = next;
+			prev->next = temp->next;
+			temp->next = head;
+			head = temp;
 		}else{
-			//Node* next = temp->next;
-			if(evenHead == NULL){
-				evenHead = temp;
-				evenTail = evenHead;
-			}else{
-				temp->next = NULL;
-				evenTail->next = temp;
-				evenTail = temp;
-			}
-			if(prev){
-				prev->next = next;
-			}
-			temp = next;
-		}		
+			prev = temp;
+		}	
+		temp = next;
 	}
-	PrintLinkedList(newHead);
-	PrintLinkedList(evenHead);
-	temp = newHead;
-	Node* evenTemp = evenHead;
-	while(temp!=NULL && evenTemp!=NULL){
-		Node* evenTempNext = evenTemp->next;
+	temp = head;
+	while(temp->data%2!=0){
+		prev=temp;
+		temp = temp->next;
+	}
+	prev->next = NULL;
+	even = temp;
+	temp = head;
+	Node* evenNode = even;
+	while(temp!=NULL && evenNode!=NULL){
 		Node* tempNext = temp->next;
-		temp->next = evenTemp;
-		evenTemp->next = tempNext;
+		Node* evenNodeNext = evenNode->next;
+		temp->next = evenNode;
+		evenNode->next = tempNext;
 		temp = tempNext;
-		evenTemp = evenTempNext;
+		evenNode = evenNodeNext;
 	}
-	return newHead;
+	return head;
 }
 //11 -> 20 -> 40 -> 55 -> 77 -> 80 -> NULL
 //10 -> 1 -> 2 -> 3 -> 5 -> 6 -> 7 -> 8 -> NULL
 int main(){
 	Node* head = NULL;
-	/*head = Push(head, 11);
+	head = Push(head, 11);
 	head = Push(head, 20);
 	head = Push(head, 40);
 	head = Push(head, 55);
 	head = Push(head, 77);
-	head = Push(head, 80);*/
+	head = Push(head, 80);
+	PrintLinkedList(head);
+	head = AlternateOddEven(head);
+	PrintLinkedList(head);
+	head = NULL;
 	head = Push(head, 10);
 	head = Push(head, 1);
 	head = Push(head, 2);
