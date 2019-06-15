f1577089f962db977995cd216e4bf6a7a9766cac
Rishabh <riddletom76@gmail.com>
2019-06-15 21:18:04 +0530

    iterative approach

---
+++ LinkedList/DeleteGreaterRight.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include"LinkedList.h"
+using namespace std;
+
+Node* DeleteGreaterRight(Node* head){
+	Node* temp = head;
+	Node* prev = NULL;
+	while(temp!=NULL && temp->next!=NULL){
+		Node* next = temp->next;
+		if(temp->data < (temp->next)->data){
+			if(temp == head){
+				head = next;
+				prev = NULL;
+			}
+			if(prev!=NULL){
+				prev->next = temp->next;
+			}
+			Node* del = temp;
+			delete(del);
+		}else{
+			prev = temp;
+		}
+		temp = next;
+	}	
+	return head;
+}
+//12->15->10->11->5->6->2->3->NULL 
+//10->20->30->40->50->60->NULL
+int main(){
+	Node* head = NULL;
+	head = Push(head, 12);
+	head = Push(head, 15);
+	head = Push(head, 10);
+	head = Push(head, 11);
+	head = Push(head, 5);
+	head = Push(head, 6);
+	head = Push(head, 2);
+	head = Push(head, 3);
+	PrintLinkedList(head);
+	head = DeleteGreaterRight(head);
+	PrintLinkedList(head);
+	Node* head2 = NULL;
+	head2 = Push(head2, 10);
+	head2 = Push(head2, 20);
+	head2 = Push(head2, 30);
+	head2 = Push(head2, 40);
+	head2 = Push(head2, 50);
+	head2 = Push(head2, 60);
+	PrintLinkedList(head2);
+	head2 = DeleteGreaterRight(head2);
+	PrintLinkedList(head2);
+}
