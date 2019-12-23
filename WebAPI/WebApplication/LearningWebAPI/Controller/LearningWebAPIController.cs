aafba43f4eec8f043856425a02c38d7c798170b3
Rishabh Sharma <riddletom76@gmail.com>
2019-12-23 18:23:14 +0530

    Web API implementation

---
+++ WebAPI/WebApplication/LearningWebAPI/Controller/LearningWebAPIController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace LearningWebAPI.Controller
+{
+    public class LearningWebAPIController : ApiController
+    {
+        static List<string> values = new List<string>()
+        {
+            "value0", "value1", "value2"
+        };
+        public IEnumerable<string> Get()
+        {
+            return values;
+        }
+
+        public string Get(int id)
+        {
+            return values[id];
+        }
+
+        public void Post([FromBody] string message)
+        {
+            values.Add(message);
+        }
+
+        public void Put(int id, [FromBody] string message)
+        {
+            values[id] = message;
+        }
+
+        public void Delete(int id)
+        {
+            values.RemoveAt(id);
+        }
+
+    }
+}
