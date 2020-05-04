fc8602e839a8e7998527325f2cf3c0c3ce20f378
rishabh <riddletom76@gmail.com>
2020-05-04 21:22:15 +0530

    string slice method

--- JavaScript/chapter_1/sandbox.js
+++ JavaScript/chapter_1/sandbox.js
@@ -1,2 +1,9 @@
-let variable_1 = null; //this is explicitly being set to null
-let variable_2; //this will get a value of undefined from the browser
+let email = "rishabhgsharma95@gmail.com";
+let result = email.slice(0,5);
+console.log(result);
+result = email.slice(2,5);
+console.log(result);
+
+//the first index in slice  determines the starting point 
+//the second index shows the end point till where the slice should happen, which means excluding
+//the element which is present at that index.
