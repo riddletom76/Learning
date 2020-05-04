611315ea79e53a82ce814b45718c903eb9c020d5
rishabh <riddletom76@gmail.com>
2020-05-04 21:28:25 +0530

    replace method

--- JavaScript/chapter_1/sandbox.js
+++ JavaScript/chapter_1/sandbox.js
@@ -1,9 +1,5 @@
 let email = "rishabhgsharma95@gmail.com";
-let result = email.substr(0, 10);   
+let result = email.replace('r', 's');
 console.log(result);
-result = email.substr(4,10);
-console.log(result);
-//substr is the substring method for the string data type
-//the first argument is the starting index for the method
-//the second argument is the number of characters it must include in the substring
-//Note: unlike slice the second argument does not correspond to the end index
+
+//the replace will only replace the first occurance of the character.
