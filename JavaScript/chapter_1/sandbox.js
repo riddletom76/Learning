3ae6bbdc245b8da20bbc899448a049630daba380
rishabh <riddletom76@gmail.com>
2020-05-04 21:57:41 +0530

    array and array functions

--- JavaScript/chapter_1/sandbox.js
+++ JavaScript/chapter_1/sandbox.js
@@ -1,10 +1,21 @@
-let email = "rishabhgsharma95@gmail.com";
-let name = "Rishabh Sharma"
+let newArray = ['rishabh', 'sharma', 'noob'];
 
-let result = name + email;
-
+let result = newArray.join(',');
 console.log(result);
-result = `${name} ${email}`;
+result = newArray.indexOf('sharma');
 console.log(result);
+result = newArray.concat('hello');
+console.log({result});
+console.log({newArray});
 
-//we can use `` to interpolate the variables in a string.
+result = newArray.push('world');
+console.log({result});
+console.log({newArray});
+result = newArray.pop();
+console.log({result});
+console.log({newArray});
+
+//push and pop are also called destructive functions for array
+//the destructive functions change the actual object on which they are working on
+//if you see concat it only concatenated and stores the result of concat operation in result
+//in case of push the newArray itself gets updated.
