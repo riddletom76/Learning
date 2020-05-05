1478c354a7f70633ae06e53395a992c7cefdd70e
rishabh <riddletom76@gmail.com>
2020-05-05 22:30:58 +0530

    foreach and callback

--- JavaScript/chapter_1/sandbox.js
+++ JavaScript/chapter_1/sandbox.js
@@ -1,6 +1,9 @@
-//functions vs methods
+//foreach and callback
 
-//the difference lies in the way we call them and where they are declared
-//in case of functions they can be declared in the same place and can be called independently
-//in case of methods the method is called with respect to an object instance i.e. method is invoked 
-//only to act on an object
+let people = ['person','person','person','person','person'];
+
+let logPerson = (person, index) => {
+    console.log(`Hello ${person} - ${index}`);
+}
+
+people.forEach(logPerson);
