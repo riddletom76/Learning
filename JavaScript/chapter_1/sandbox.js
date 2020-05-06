f75992b47f85dfe2bae893888d0d1ed53ddd27f9
rishabh <riddletom76@gmail.com>
2020-05-06 08:18:32 +0530

    populating ul and displaying on front end

--- JavaScript/chapter_1/sandbox.js
+++ JavaScript/chapter_1/sandbox.js
@@ -1,9 +1,12 @@
 //foreach and callback
 
 let people = ['person','person','person','person','person'];
-
+let html = "";
 let logPerson = (person, index) => {
-    console.log(`Hello ${person} - ${index}`);
+    html += (`<li>Hello ${person} - ${index}</li>`);
 }
 
 people.forEach(logPerson);
+
+let ul = document.getElementById("list");
+ul.innerHTML = html;
