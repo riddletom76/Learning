4e15b5d69e31934010c0e1511bc4374fdadef7cd
rishabh <riddletom76@gmail.com>
2020-05-18 23:49:52 +0530

    todo phase 2 initial

---
+++ React/hello-world/src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TodoItem(){
+    return(
+        <div>
+            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+            <label for="vehicle1"> I have a bike</label><br />
+        </div>
+    )
+}
+export default TodoItem
