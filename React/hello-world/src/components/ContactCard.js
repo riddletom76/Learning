c67a3abe6a158cacc1ac299a25eae5650b3023d9
Rishabh Sharma <riddletom76@gmail.com>
2020-05-25 15:30:21 +0530

    passing the contact info in json format to the component

--- React/hello-world/src/components/ContactCard.js
+++ React/hello-world/src/components/ContactCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/ContactCard.css";
 function ContactCard(props){
+    console.log(props);
     return (
         <div class="contact-card">
-            <p>My name is: {props.Name}</p>
-            <p>My age is: {props.Age}</p>
-            <p>My number is: {props.Number}</p>
+            <p>My name is: {props.contact.Name}</p>
+            <p>My age is: {props.contact.Age}</p>
+            <p>My number is: {props.contact.Number}</p>
         </div>
     );
 }
--- React/hello-world/src/components/ContactInfo.js
+++ React/hello-world/src/components/ContactInfo.js
@@ -3,10 +3,10 @@ import ContactCard from "./ContactCard";
 
 function ContactInfo(){
     return (<div>
-        <ContactCard Name="Rishabh" Age='20' Number="11111072" />
-        <ContactCard Name="Sharma" Age='20' Number="11111073" />
-        <ContactCard Name="Ram" Age='20' Number="1111111074" />
-        <ContactCard Name="Shyam" Age='20' Number="111111075" />
+        <ContactCard contact={{Name:"Rishabh",  Age:'20', Number:"11111072"}}/>
+        <ContactCard contact={{Name:"Sharma", Age:'20', Number:"11111073"}} />
+        <ContactCard contact={{Name:"Ram", Age:'20', Number:"1111111074"}} />
+        <ContactCard contact={{Name:"Shyam", Age:'20', Number:"111111075"}} />
     </div>);
 }
 
