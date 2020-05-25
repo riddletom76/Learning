7c54595d92f7f93ecefdfed8f1cd47c34de59af0
Rishabh Sharma <riddletom76@gmail.com>
2020-05-25 14:17:45 +0530

    popup using javascript

--- JavaScript/chapter_1/sandbox.js
+++ JavaScript/chapter_1/sandbox.js
@@ -1,19 +1,11 @@
-//what is DOM?
-//QuerySelector?
-//use a queryselector to insert an unordered list in the html page
-//getting and setting attributes of a tag
-//use the queryselector to set a class which will have an associated css attribute set
-//add and remove classes
+document.getElementById("btn_open").addEventListener("click", showPopup);
+document.querySelector('.close').addEventListener('click', hidePopup);
+function showPopup(){
+    var content = document.getElementById("overlay");
+    content.style.visibility = "visible";
+}
 
-let bodyElement = document.querySelector('#list');
-console.log(bodyElement);
-let list = document.createElement('ul');
-let item = document.createElement('li');
-item.innerHTML = "first item";
-list.appendChild(item);
-document.getElementById('list').appendChild(list);
-bodyElement.setAttribute("class", 'rishabh')
-console.log(bodyElement.getAttribute('class'))
-let classList = document.getElementById('list').classList;
-classList.add('sharma');
-console.log(bodyElement.getAttribute('class'))
+function hidePopup(){
+    var content = document.getElementById("overlay");
+    content.style.visibility = "hidden";
+}
