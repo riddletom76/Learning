2dfbfdcabb821e930be628b23a9585cbad82dc4e
Rishabh Sharma <riddletom76@gmail.com>
2020-05-27 21:27:35 +0530

    adding state to the App component

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -12,9 +12,19 @@ import Product from './components/Product';
 // }
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      name: "Rishabh Sharma",
+      age: 20
+    }
+  }
   render(){
     return(
-      <Main />
+      <div>
+        <h1>I am {this.state.name}</h1>
+        <h1>{this.state.age} years old</h1>
+      </div>
     )
   }
 }
