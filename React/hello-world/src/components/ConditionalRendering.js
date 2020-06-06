4fd5d7627fc2364096c011da12b250e5a615b71a
Rishabh Sharma <riddletom76@gmail.com>
2020-06-06 19:16:47 +0530

    Conditional rendering for react components

---
+++ React/hello-world/src/components/ConditionalRendering.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class ConditionalRendering extends React.Component
+{
+    constructor(){
+        super();
+        this.state = {
+            isloading: true
+        }
+    }
+
+    componentDidMount(){
+        setTimeout(()=>{
+            this.setState({isloading: false})}, 1500)
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.isloading ? <p>Loading</p> : <p>Loaded</p>}
+            </div>
+        )
+    }
+}
+
+export default ConditionalRendering
